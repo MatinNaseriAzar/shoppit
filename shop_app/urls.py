1ebdb3f33baf0cc4394cdc533079c41b51086e3f
MatinNaseriAzar <matinnaseriazar58@gmail.com>
2025-11-25 23:36:55 +0330

    Refactor views to use generic views and update urls

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -1,24 +1,30 @@
-from django.urls import path
-from . import views
-from .views import products
+from django.urls import path , include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+from .views import (
+    ProductListView,
+    ProductDetailView,
+    AddItemView,
+    ProductInCartView,
+    CartStartView,
+    CartDetailView,
+    UpdateQuantityView,
+    DeleteCartItemView,
+    UsernameView,
+    UserInfoView
+)
 
-router = DefaultRouter()
-router.register("products",ProductViewSet, basename="products")
 
 urlpatterns = [
+    path("products/", ProductListView.as_view() , name="product_list"),
 
-    router.urls,
+    path("product_detail/<slug:slug>/", ProductDetailView.as_view(), name="product_detail"),
+    path("add_item/", AddItemView.as_view(), name="add_item"),
+    path("product_in_cart/", ProductInCartView.as_view(), name="product_in_cart"),
+    path("get_cart_start/", CartStartView.as_view(), name="get_cart_start"),
+    path("get_cart/", CartDetailView.as_view(), name="get_cart"),
+    path("update_quantity/", UpdateQuantityView.as_view(), name="update_quantity"),
+    path("delete_cartitem/", DeleteCartItemView.as_view(), name="delete_cartitem"),
 
-    path("product_detail/<slug:slug>/",views.product_detail,name='product_detail'),
-    path("add_item/",views.add_item,name="add_item"),
-    path("product_in_cart/",views.product_in_cart,name="product_in_cart"),
-    path("get_cart_start/",views.get_cart_start,name="get_cart_start"),
-    path("get_cart/",views.get_cart,name="get_cart"),
-    path("update_quantity/",views.update_quantity,name="update_quantity"),
-    path("delete_cartitem/",views.delete_cartitem,name="delete_cartitem"),
-    path("get_username/",views.get_username,name="get_username"),
-    path("user_info/",views.user_info,name="user_info"),
-    # path("initiate_payment/",views.initiate_payment,name="initiate_payment")
+    path("get_username/", UsernameView.as_view(), name="get_username"),
+    path("user_info/", UserInfoView.as_view(), name="user_info"),
 ]
--- shop_app/views.py
+++ shop_app/views.py
@@ -5,110 +5,213 @@ from .serializers import ProductSerializer , DetailProductSerializer , CartItemS
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from decimal import Decimal
-import uuid
+from rest_framework.generics import GenericAPIView
+from rest_framework import mixins 
 from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import RetrieveAPIView , CreateAPIView , UpdateAPIView , DestroyAPIView , ListAPIView
 
 
 BASE_URL = ""
 
-class ProductViewSet(ModelViewSet):
+class ProductListView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['category']
 
-@api_view(["GET"])
-def product_detail(request,slug):
-    product = Product.objects.get(slug=slug)
-    serializer = DetailProductSerializer(product)
-    return Response(serializer.data)
 
-@api_view(["POST"])
-def add_item(request):
-    try :
-        cart_code = request.data.get("cart_code")
-        product_id = request.data.get("product_id")
-
-        #پیدا کردن Cart با cart_code 
-        cart , created = Cart.objects.get_or_create(cart_code = cart_code)
-        product=  Product.objects.get(id=product_id)
-
-        cartitem , created = CartItem.objects.get_or_create(cart=cart , product=product)
-        cartitem.quantity = 1
-        cartitem.save()
-
-        serializer = CartItemSerializer(cartitem)
-        return Response({"detail":serializer.data, "message": "CartItem created succcessfully"} , status=201)
-    
-    except Exception as e :
-        return Response({"error": str(e)}, status=400)
+class ProductDetailView(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = DetailProductSerializer
+    lookup_field = 'slug'
     
 
-@api_view(["GET"])
-def product_in_cart(request):
-    cart_code = request.query_params.get("cart_code")
-    product_id = request.query_params.get("product_id")
+# @api_view(["POST"])
+# def add_item(request):
+#     try :
+#         cart_code = request.data.get("cart_code")
+#         product_id = request.data.get("product_id")
+
+#         #پیدا کردن Cart با cart_code 
+#         cart , created = Cart.objects.get_or_create(cart_code = cart_code)
+#         product=  Product.objects.get(id=product_id)
+
+#         cartitem , created = CartItem.objects.get_or_create(cart=cart , product=product)
+#         cartitem.quantity = 1
+#         cartitem.save()
+
+#         serializer = CartItemSerializer(cartitem)
+#         return Response({"detail":serializer.data, "message": "CartItem created succcessfully"} , status=201)
     
-    cart = Cart.objects.get(cart_code=cart_code)
-    product = Product.objects.get(id=product_id)
+#     except Exception as e :
+#         return Response({"error": str(e)}, status=400)
+    
+class AddItemView(CreateAPIView):
+    serializer_class = CartItemSerializer
+    queryset = CartItem.objects.all()
 
-    product_exists_in_cart = CartItem.objects.filter(cart=cart,product=product).exists()
+    def create(self,request,*args,**kwargs):
+        try:
+            cart_code = request.data.get("cart_code")
+            product_id = request.data.get("product_id")
 
-    return Response({'product_in_cart':product_exists_in_cart})
+            cart , created = Cart.objects.get_or_create(cart_code = cart_code)
+            product = Product.objects.get(id=product_id)
 
-@api_view(["GET"])
-def get_cart_start(request):
-    cart_code = request.query_params.get("cart_code")
-    cart = Cart.objects.get(cart_code=cart_code,paid=False)
-    serializer = CartSimpleSerializer(cart)
-    return Response(serializer.data)
+            cartitem , created = CartItem.objects.get_or_create(
+                cart=cart , product = product
+            )
+
+            cartitem.quantity = 1
+            cartitem.save()
+
+            serializer = CartItemSerializer(cartitem)
+            return Response(
+                {"detail":serializer.data, "message" : "CartItem created successfully"},
+                status=201
+            )
+        except Exception as  e :
+            return Response({"error":str(e)},status=400)
 
 
-@api_view(["GET"])
-def get_cart(request):
-    cart_code = request.query_params.get("cart_code")
-    cart = Cart.objects.get(cart_code=cart_code,paid=False)
-    serializer = CartSerializer(cart)
-    return Response(serializer.data)
+# @api_view(["GET"])
+# def product_in_cart(request):
+#     cart_code = request.query_params.get("cart_code")
+#     product_id = request.query_params.get("product_id")
+    
+#     cart = Cart.objects.get(cart_code=cart_code)
+#     product = Product.objects.get(id=product_id)
+
+#     product_exists_in_cart = CartItem.objects.filter(cart=cart,product=product).exists()
+
+#     return Response({'product_in_cart':product_exists_in_cart})
+
+class ProductInCartView(GenericAPIView):
+    def get(self,request):
+        cart_code= request.query_params.get("cart_code")
+        product_id = request.query_params.get("product_id")
+
+        cart = Cart.objects.get(cart_code = cart_code)
+        product = Product.objects.get(id=product_id)
+
+        exists = CartItem.objects.filter(cart=cart, product=product).exists()
+
+        return Response({"product_in_cart":exists})
+
+# @api_view(["GET"])
+# def get_cart_start(request):
+#     cart_code = request.query_params.get("cart_code")
+#     cart = Cart.objects.get(cart_code=cart_code,paid=False)
+#     serializer = CartSimpleSerializer(cart)
+#     return Response(serializer.data)
+
+class CartStartView(RetrieveAPIView):
+    serializer_class = CartSimpleSerializer
+
+    def get_object(self):
+        cart_code = self.request.query_params.get("cart_code")
+        return Cart.objects.get(cart_code=cart_code,paid=False)
 
 
-@api_view(["PATCH"])
-def update_quantity(request):
-    try:
-        cartitem_id = request.data.get("item_id")
-        quantity = request.data.get("quantity")
-        quantity = int(quantity)
-        cartitem = CartItem.objects.get(id=cartitem_id)
-        cartitem.quantity = quantity
-        cartitem.save()
-        serializer = CartItemSerializer(cartitem)
-        return Response({"data":serializer.data ,"message": "Cartitem  updated successfully"})
-    except Exception as e :
-        return Response({"error":str(e),},status=400)
+# @api_view(["GET"])
+# def get_cart(request):
+#     cart_code = request.query_params.get("cart_code")
+#     cart = Cart.objects.get(cart_code=cart_code,paid=False)
+#     serializer = CartSerializer(cart)
+#     return Response(serializer.data)
+
+class CartDetailView(RetrieveAPIView):
+    serializer_class = CartSerializer
+
+    def get_object(self):
+        cart_code = self.request.query_params.get("cart_code")
+        return Cart.objects.get(cart_code=cart_code,paid = False)
+
+
+# @api_view(["PATCH"])
+# def update_quantity(request):
+#     try:
+#         cartitem_id = request.data.get("item_id")
+#         quantity = request.data.get("quantity")
+#         quantity = int(quantity)
+#         cartitem = CartItem.objects.get(id=cartitem_id)
+#         cartitem.quantity = quantity
+#         cartitem.save()
+#         serializer = CartItemSerializer(cartitem)
+#         return Response({"data":serializer.data ,"message": "Cartitem  updated successfully"})
+#     except Exception as e :
+#         return Response({"error":str(e),},status=400)
     
 
-@api_view(["DELETE"])
-def delete_cartitem(request):
-    cartitem_id = request.data.get("item_id")
-    cartitem = CartItem.objects.get(id=cartitem_id)
-    cartitem.delete()
-    return Response({"message":"item deleted successfully"},status=status.HTTP_204_NO_CONTENT)
+class UpdateQuantityView(UpdateAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def get_username(request):
-    user = request.user
-    return Response({"username": user.username})
+    def update(self,request,*args,**kwargs):
+        try :
+            item_id = request.data.get("item_id")
+            quantity = int(request.data.get("quantity"))
+
+            CartItem = CartItem.objects.get(id=item_id)
+            CartItem.quantity = quantity
+            CartItem.save()
+
+            serializer = CartItemSerializer(CartItem)
+            return Response({"data":serializer.data, "message" : "updated successfully"})
+        except Exception as e :
+            return Response({"error":str(e)},status=400)
 
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def user_info(request):
-    user=request.user
-    serializer = UserSerializer(user)
-    return  Response(serializer.data)
+# @api_view(["DELETE"])
+# def delete_cartitem(request):
+#     cartitem_id = request.data.get("item_id")
+#     cartitem = CartItem.objects.get(id=cartitem_id)
+#     cartitem.delete()
+#     return Response({"message":"item deleted successfully"},status=status.HTTP_204_NO_CONTENT)
+
+
+class DeleteCartItemView(DestroyAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+    def delete(self,request,*args,**kwargs):
+        item_id = request.data.get("item_id")
+        cartitem = CartItem.objects.get(id=item_id)
+        cartitem.delete()
+        return Response({"message":"item deleted successfully"}, status=204)
+    
+
+# @api_view(["GET"])
+# @permission_classes([IsAuthenticated])
+# def get_username(request):
+#     user = request.user
+#     return Response({"username": user.username})
+
+class UsernameView(RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+    
+    def retrieve(self, request, *args, **kwargs):
+        return Response({"username":request.user.username})
+
+
+# @api_view(["GET"])
+# @permission_classes([IsAuthenticated])
+# def user_info(request):
+#     user=request.user
+#     serializer = UserSerializer(user)
+#     return  Response(serializer.data)
+
+class UserInfoView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
 
 # @api_view(["GET"])
 # @permission_classes([IsAuthenticated])
